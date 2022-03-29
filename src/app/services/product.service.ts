7274a960aaefc9221c408327e14ce1260d709bc0
sztamas123 <szalma_tamas@yahoo.com>
2022-03-29 10:11:46 +0300

    Product detailed view done

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -39,6 +39,12 @@ export class ProductService {
       map(response => response._embedded.products)
     );
   }
+
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
 }
 
 interface GetResponseProducts {
