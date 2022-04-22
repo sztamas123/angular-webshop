5bc57d8aa04a45202a54916f05832ece9f481e9b
sztamas123 <szalma_tamas@yahoo.com>
2022-04-22 13:52:17 +0300

    Deployment works need to fix login to work on http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  webshopApiUrl: "http://localhost:8443/api",
+  webshopApiUrl: "https://192.168.49.2:30163/api",
   stripePublishableKey: "pk_test_51KlqZ9Hms9BiXn5Y99EZWIlviHXyqQu28CdOGiggi6AVnpqTDqrPGyKcSF9dWlOmTtC67wRe2MbNgGEqLFmbCsOq00gGqQlq4h"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  webshopApiUrl: "http://localhost:8443/api",
+  webshopApiUrl: "https://192.168.49.2:30163/api",
   stripePublishableKey: "pk_test_51KlqZ9Hms9BiXn5Y99EZWIlviHXyqQu28CdOGiggi6AVnpqTDqrPGyKcSF9dWlOmTtC67wRe2MbNgGEqLFmbCsOq00gGqQlq4h"
 };
 
